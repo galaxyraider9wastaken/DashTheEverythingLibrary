52dffdc0de2eb86d457f8cb2947f23b7dec78154
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-03 14:46:33 -0700

    Update Vector.cs

--- src/LinearAlgebra/Vector.cs
+++ src/LinearAlgebra/Vector.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace Dash{
+  public class Vector2 {
+    public float X;
+    public float Y;
+    public Vector2(float x, float y){
+      X = x;
+      Y = y;
+    }
+  }
+}
