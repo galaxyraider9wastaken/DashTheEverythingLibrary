744508c42dc5e0661b5f917e4da6c0de02b03558
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-04 12:42:51 -0700

    Update Logger.cs

--- src/BugHelper/Logger.cs
+++ src/BugHelper/Logger.cs
@@ -8,15 +8,26 @@ namespace Dash{
     public Log(string directory){
       Directory = directory;
     }
-    public void LoggerStartTime(){
+    public void LoggerStart(){
       ElapsedTime = new Stopwatch();
       Stopwatch.Start();
-    }
-    public void Log(string FileFrom, string Log){
-      var Et = ElapesedTime.Elapsed;
-      using (StreamWriter writer = new StreamWriter(fullPath))  
+      using (StreamWriter writer = new StreamWriter(Directory, true))  
       {  
-        writer.WriteLine(Et.Hours + " : " + Et.Minutes + " : " + Et.Seconds);
+        writer.WriteLine(|| + " Log started " + ||);
+      }  
+    }
+    public void Log(string FileFrom, int Line, string Log){
+      var Et = ElapesedTime.Elapsed;
+      using (StreamWriter writer = new StreamWriter(Directory, true))  
+      {  
+        writer.WriteLine(Et.Hours + " : " + Et.Minutes + " : " + Et.Seconds + " || " + FileFrom + " | " + Line + " || " + Log);
+      }  
+    }
+    public void LoggerEnd(){
+      Stopwatch.End();
+      using (StreamWriter writer = new StreamWriter(Directory, true))  
+      {  
+        writer.WriteLine(|| + " Log Ended " + || + " { Elapsed Time } " + Et.Hours + " : " + Et.Minutes + " : " + Et.Seconds);
       }  
     }
   }
