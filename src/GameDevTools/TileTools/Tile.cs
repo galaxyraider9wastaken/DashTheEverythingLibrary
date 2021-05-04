8d90bc6b4412ad33ba6a767fc2aa07ada0f8d99a
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-04 12:18:06 -0700

    Update Tile.cs

--- src/GameDevTools/TileTools/Tile.cs
+++ src/GameDevTools/TileTools/Tile.cs
@@ -1,1 +1,12 @@
-
+  
+using System;
+namespace Dash{
+  public class Tile {
+    public int Id;
+    public bool Collidable;
+    public Tile(int id, bool collidable){
+      Id = id;
+      Collidable = collidable;
+    }
+  }
+}
