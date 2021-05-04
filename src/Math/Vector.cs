64d9670f3d75d5e99ca1b45b88f040daa5248f8a
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-04 14:11:40 -0700

    Update Vector.cs

--- src/Math/Vector.cs
+++ src/Math/Vector.cs
@@ -3,20 +3,20 @@ using System;
 namespace Dash{
   public class Vector {
     public static Vector2 AddVec2(Vector2 V1, Vector2 V2){
-      var VecA = new Vector2(0, 0)
+      var VecA = new Vector2(0, 0);
       VecA.X = V1.X +V2.X;
       VecA.Y = V1.Y +V2.Y;
       return VecA;
     }
     public static Vector3 AddVec3(Vector3 V1, Vector3 V2){
-      var VecA = new Vector3(0, 0, 0)
+      var VecA = new Vector3(0, 0, 0);
       VecA.X = V1.X +V2.X;
       VecA.Y = V1.Y +V2.Y;
       VecA.Z = V1.Z +V2.Z;
       return VecA;
     }
     public static Vector4 AddVec4(Vector4 V1, Vector4 V2){
-      var VecA = new Vector4(0, 0, 0, 0)
+      var VecA = new Vector4(0, 0, 0, 0);
       VecA.X = V1.X +V2.X;
       VecA.Y = V1.Y +V2.Y;
       VecA.Z = V1.Z +V2.Z;
@@ -24,20 +24,20 @@ namespace Dash{
       return VecA;
     }
      public static Vector2 SubtractVec2(Vector2 V1, Vector2 V2){
-      var VecA = new Vector2(0, 0)
+      var VecA = new Vector2(0, 0);
       VecA.X = V1.X -V2.X;
       VecA.Y = V1.Y -V2.Y;
       return VecA;
     }
     public static Vector3 SubtractVec3(Vector3 V1, Vector3 V2){
-      var VecA = new Vector3(0, 0, 0)
+      var VecA = new Vector3(0, 0, 0);
       VecA.X = V1.X -V2.X;
       VecA.Y = V1.Y -V2.Y;
       VecA.Z = V1.Z -V2.Z;
       return VecA;
     }
     public static Vector4 SubtractVec4(Vector4 V1, Vector4 V2){
-      var VecA = new Vector4(0, 0, 0, 0)
+      var VecA = new Vector4(0, 0, 0, 0);
       VecA.X = V1.X -V2.X;
       VecA.Y = V1.Y -V2.Y;
       VecA.Z = V1.Z -V2.Z;
@@ -45,20 +45,20 @@ namespace Dash{
       return VecA;
     }
     public static Vector2 MultiplyVec2(Vector2 V1, Vector2 V2){
-      var VecA = new Vector2(0, 0)
+      var VecA = new Vector2(0, 0);
       VecA.X = V1.X * V2.X;
       VecA.Y = V1.Y * V2.Y;
       return VecA;
     }
     public static Vector3 MultiplyVec3(Vector3 V1, Vector3 V2){
-      var VecA = new Vector3(0, 0, 0)
+      var VecA = new Vector3(0, 0, 0);
       VecA.X = V1.X * V2.X;
       VecA.Y = V1.Y * V2.Y;
       VecA.Z = V1.Z * V2.Z;
       return VecA;
     }
     public static Vector4 MultiplyVec4(Vector4 V1, Vector4 V2){
-      var VecA = new Vector4(0, 0, 0, 0)
+      var VecA = new Vector4(0, 0, 0, 0);
       VecA.X = V1.X * V2.X;
       VecA.Y = V1.Y * V2.Y;
       VecA.Z = V1.Z * V2.Z;
@@ -66,20 +66,20 @@ namespace Dash{
       return VecA;
     }
      public static Vector2 DivideVec2(Vector2 V1, Vector2 V2){
-      var VecA = new Vector2(0, 0)
+      var VecA = new Vector2(0, 0);
       VecA.X = V1.X / V2.X;
       VecA.Y = V1.Y / V2.Y;
       return VecA;
     }
     public static Vector3 DivideVec3(Vector3 V1, Vector3 V2){
-      var VecA = new Vector3(0, 0, 0)
+      var VecA = new Vector3(0, 0, 0);
       VecA.X = V1.X / V2.X;
       VecA.Y = V1.Y / V2.Y;
       VecA.Z = V1.Z / V2.Z;
       return VecA;
     }
     public static Vector4 DivideVec4(Vector4 V1, Vector4 V2){
-      var VecA = new Vector4(0, 0, 0, 0)
+      var VecA = new Vector4(0, 0, 0, 0);
       VecA.X = V1.X / V2.X;
       VecA.Y = V1.Y / V2.Y;
       VecA.Z = V1.Z / V2.Z;
@@ -116,7 +116,7 @@ namespace Dash{
     public float Y;
     public float Z;
     public float W;
-    public Vector4(float x, float y, float w){
+    public Vector4(float x, float y, float z, float w){
       X = x;
       Y = y;
       Z = z;
