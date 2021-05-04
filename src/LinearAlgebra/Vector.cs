0a9a2a8ef404976f27864a8ee39a61eb56152be7
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-04 10:58:51 -0700

    Update Vector.cs

--- src/LinearAlgebra/Vector.cs
+++ src/LinearAlgebra/Vector.cs
@@ -4,23 +4,65 @@ namespace Dash{
   public class Vector {
     public static Vector2 AddVec2(Vector2 V1, Vector2 V2){
       var VecA = new Vector2(0, 0)
-      VecA.X = V1.X +=V2.X;
-      VecA.Y = V1.Y +=V2.Y;
+      VecA.X = V1.X +V2.X;
+      VecA.Y = V1.Y +V2.Y;
       return VecA;
     }
     public static Vector3 AddVec3(Vector3 V1, Vector3 V2){
       var VecA = new Vector3(0, 0, 0)
-      VecA.X = V1.X +=V2.X;
-      VecA.Y = V1.Y +=V2.Y;
-      VecA.Z = V1.Z +=V2.Z;
+      VecA.X = V1.X +V2.X;
+      VecA.Y = V1.Y +V2.Y;
+      VecA.Z = V1.Z +V2.Z;
       return VecA;
     }
     public static Vector4 AddVec4(Vector4 V1, Vector4 V2){
       var VecA = new Vector4(0, 0, 0, 0)
-      VecA.X = V1.X +=V2.X;
-      VecA.Y = V1.Y +=V2.Y;
-      VecA.Z = V1.Z +=V2.Z;
-      VecA.W = V1.Z +=V2.W;
+      VecA.X = V1.X +V2.X;
+      VecA.Y = V1.Y +V2.Y;
+      VecA.Z = V1.Z +V2.Z;
+      VecA.W = V1.Z +V2.W;
+      return VecA;
+    }
+     public static Vector2 SubtractVec2(Vector2 V1, Vector2 V2){
+      var VecA = new Vector2(0, 0)
+      VecA.X = V1.X -V2.X;
+      VecA.Y = V1.Y -V2.Y;
+      return VecA;
+    }
+    public static Vector3 SubtractVec3(Vector3 V1, Vector3 V2){
+      var VecA = new Vector3(0, 0, 0)
+      VecA.X = V1.X -V2.X;
+      VecA.Y = V1.Y -V2.Y;
+      VecA.Z = V1.Z -V2.Z;
+      return VecA;
+    }
+    public static Vector4 SubtractVec4(Vector4 V1, Vector4 V2){
+      var VecA = new Vector4(0, 0, 0, 0)
+      VecA.X = V1.X -V2.X;
+      VecA.Y = V1.Y -V2.Y;
+      VecA.Z = V1.Z -V2.Z;
+      VecA.W = V1.Z -V2.W;
+      return VecA;
+    }
+    public static Vector2 MultiplyVec2(Vector2 V1, Vector2 V2){
+      var VecA = new Vector2(0, 0)
+      VecA.X = V1.X * V2.X;
+      VecA.Y = V1.Y * V2.Y;
+      return VecA;
+    }
+    public static Vector3 MultiplyVec3(Vector3 V1, Vector3 V2){
+      var VecA = new Vector3(0, 0, 0)
+      VecA.X = V1.X * V2.X;
+      VecA.Y = V1.Y * V2.Y;
+      VecA.Z = V1.Z * V2.Z;
+      return VecA;
+    }
+    public static Vector4 MultiplyVec4(Vector4 V1, Vector4 V2){
+      var VecA = new Vector4(0, 0, 0, 0)
+      VecA.X = V1.X * V2.X;
+      VecA.Y = V1.Y * V2.Y;
+      VecA.Z = V1.Z * V2.Z;
+      VecA.W = V1.Z * V2.W;
       return VecA;
     }
   }
