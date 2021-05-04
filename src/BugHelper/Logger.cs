64188a89ac9be53909cb703e91170727c8b39e6b
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2021-05-04 12:35:41 -0700

    Update Logger.cs

--- src/BugHelper/Logger.cs
+++ src/BugHelper/Logger.cs
@@ -1,19 +1,22 @@
 using System;
+using System.Diagnostics;
 
 namespace Dash{
   public class Logger {
     public string Directory;
-    public int LoggerTime;
+    public Stopwatch ElapsedTime;
     public Log(string directory){
       Directory = directory;
     }
     public void LoggerStartTime(){
-      
+      ElapsedTime = new Stopwatch();
+      Stopwatch.Start();
     }
     public void Log(string FileFrom, string Log){
+      var Et = ElapesedTime.Elapsed;
       using (StreamWriter writer = new StreamWriter(fullPath))  
       {  
-        
+        writer.WriteLine(Et.Hours + " : " + Et.Minutes + " : " + Et.Seconds);
       }  
     }
   }
